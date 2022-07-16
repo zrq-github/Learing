73361fe11adc83a67febb053aaaa29057c3679a4
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-07-16 17:24:13 +0800

    更新到建议57

---
+++ 编写高质量代码改善C#程序的157个建议/建议46-显式释放资源/AnotherResource.cs
@@ -0,0 +1,10 @@
+﻿namespace 建议46_显式释放资源
+{
+    internal class AnotherResource : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 编写高质量代码改善C#程序的157个建议/建议46-显式释放资源/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 建议46_显式释放资源
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
