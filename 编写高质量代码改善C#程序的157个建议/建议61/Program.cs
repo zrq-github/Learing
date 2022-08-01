de8937d796bdec69e88700738f3aca0b08d02772
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-08-01 23:30:13 +0800

    建议61的代码

---
+++ 编写高质量代码改善C#程序的157个建议/建议61/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace 建议61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int returnBelowFinally = TestIntReturnBelowFinally();
+            Console.WriteLine($"TestIntReturnBelowFinally,{returnBelowFinally}");
+
+            int returnInTry = TestIntReturnInTry();
+            Console.WriteLine($"TestIntReturnInTry,{returnInTry}");
+
+            User user = TestUserReturnInTry();
+            Console.WriteLine($"TestIntReturnInTry,{user}");
+
+            User user2 = TestUserReturnInTry2();
+            Console.WriteLine($"TestIntReturnInTry,{user2}");
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+
+        private static int TestIntReturnBelowFinally()
+        {
+            int i;
+            try
+            {
+                i = 1;
+            }
+            finally
+            {
+                i = 2;
+                Console.WriteLine("\t将int结果改为2，finally执行完毕");
+            }
+            return i;
+        }
+
+        private static int TestIntReturnInTry()
+        {
+            int i;
+            try
+            {
+                return i = 1;
+            }
+            finally
+            {
+                i = 2;
+                Console.WriteLine("\t将int结果改为2，finally执行完毕");
+            }
+        }
+
+        static User TestUserReturnInTry()
+        {
+            User user = new User() { Name = "Mike", BirthDay = new DateTime(2010, 1, 1) };
+            try
+            {
+                return user;
+            }
+            finally
+            {
+                user.Name = "Rose";
+                user.BirthDay = new DateTime(2010, 2, 2);
+                Console.WriteLine("\t将user.Name改为Rose");
+            }
+        }
+
+        private static User TestUserReturnInTry2()
+        {
+            User user = new User() { Name = "Mike", BirthDay = new DateTime(2010, 1, 1) };
+            try
+            {
+                return user;
+            }
+            finally
+            {
+                user.Name = "Rose";
+                user.BirthDay = new DateTime(2010, 2, 2);
+                user = null;
+                Console.WriteLine("\t将user置为null");
+            }
+        }
+    }
+}
---
+++ 编写高质量代码改善C#程序的157个建议/建议61/User.cs
@@ -0,0 +1,8 @@
+﻿namespace 建议61
+{
+    internal class User
+    {
+        public string Name { get; internal set; }
+        public DateTime BirthDay { get; internal set; }
+    }
+}
